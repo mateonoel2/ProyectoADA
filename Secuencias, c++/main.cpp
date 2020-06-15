4dd3fec89339093443d13c8f78206fc9cbcdb2bc
jpmiraval <40766473+jpmiraval@users.noreply.github.com>
2020-06-14 21:29:22 -0500

    voraz Done

--- Secuencias, c++/main.cpp
+++ Secuencias, c++/main.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 int p;
 
+vector <pair<int, int>> matching_voraz;
+
 template <class T>
 void print_vector(const vector<T>& vec){
     for(auto c: vec)
@@ -10,7 +12,84 @@ void print_vector(const vector<T>& vec){
     cout<<'\n';
 }
 
-void Min_voraz(const vector<int>& A,const vector<int>& B){}
+void conectar(int A, int B){
+    auto* paircito = new pair<int, int>(A, B);
+    matching_voraz.push_back(*paircito);
+    delete paircito;
+}
+void ResetConection(int &ACurrent, int &BCurrent, int &BCWeight, int&ACWeigth, int A[], int B[], bool &divided, bool &combined, float &weight){
+    ACurrent++;
+    BCurrent++;
+    conectar(A[ACurrent], B[BCurrent]);
+    ACWeigth = A[ACurrent];
+    BCWeight = B[BCurrent];
+    divided = 0; combined = 0;
+}
+
+float Min_voraz(const vector<int>& A,const vector<int>& B){
+    
+    int sizeA = A.size(), sizeB = B.size();
+    float weight = 0;
+    bool divided = false, combined = false;
+    conectar(A[0], B[0]);
+    int ACurrent = 0, BCurrent = 0;
+    int ACWeigth = A[1], BCWeight = B[1];
+    
+    for(int i = 1; i < sizeA; i++){
+        if(BCurrent < sizeB-1 && ACurrent < sizeA-1){
+            if(ACWeigth < BCWeight){
+                if(ACurrent < sizeA -2){
+                if(divided) {
+                    weight += float(ACWeigth) / float(BCWeight);
+                    ResetConection(ACurrent, BCurrent, BCWeight, ACWeigth, A, B, divided, combined, weight);
+                }else if (ACWeigth + A[i] < BCWeight){
+                    combined = 1; divided = 0;
+                    ACurrent++;
+                    conectar(A[ACurrent], B[BCurrent]);
+                    ACWeigth+=A[ACurrent];
+                }else{ 
+                    weight += float(ACWeigth) / float(BCWeight);
+                    ResetConection(ACurrent, BCurrent, BCWeight, ACWeigth, A, B, divided, combined, weight);
+                }}
+                else{
+                    weight += float(ACWeigth) / float(BCWeight);
+                    ResetConection(ACurrent, BCurrent, BCWeight, ACWeigth, A, B, divided, combined, weight);
+                }
+            }
+            else{
+                if(ACurrent < sizeA-2){
+                if(divided){
+                    weight += float(ACWeigth) / float(BCWeight);
+                    ResetConection(ACurrent, BCurrent, BCWeight, ACWeigth, A, B, divided, combined, weight);
+                }
+                else if(BCurrent < sizeB-2){
+                    BCurrent++;
+                    i--;
+                    conectar(A[ACurrent], B[BCurrent]);
+                    divided =1, combined=0;
+                }else{
+                    weight += float(ACWeigth) / float(BCWeight);
+                    ResetConection(ACurrent, BCurrent, BCWeight, ACWeigth, A, B, divided, combined, weight);
+                }}
+                else{
+                    weight += float(ACWeigth) / float(BCWeight);
+                    ResetConection(ACurrent, BCurrent, BCWeight, ACWeigth, A, B, divided, combined, weight);
+                }
+            }
+        }
+        else if (BCurrent == sizeB-1){
+            ACurrent++;
+            conectar(A[ACurrent], B[BCurrent]);
+        }
+        else{
+            BCurrent++;
+            conectar(A[ACurrent], B[BCurrent]);
+            if(BCurrent == sizeB-1)break;
+        }
+    }
+    weight += float(ACWeigth) / float(BCWeight);
+    return weight;
+}
 
 int Min_recursivo(vector<int> A, vector<int> B, int a, int b){
     A.erase(A.begin(), A.begin() + a);
