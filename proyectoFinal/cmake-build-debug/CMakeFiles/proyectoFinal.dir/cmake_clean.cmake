e596ea1593f4d1e210ee43ac0c2c5f2eb8bde813
Jean <jean.miraval@utec.edu.pe>
2020-07-25 09:26:32 -0500

    Finished

---
+++ proyectoFinal/cmake-build-debug/CMakeFiles/proyectoFinal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proyectoFinal.dir/main.cpp.o"
+  "proyectoFinal"
+  "proyectoFinal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proyectoFinal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
